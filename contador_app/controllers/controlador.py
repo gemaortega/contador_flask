0667502a2850bef1e4f382fb5921c18d841ec9e7
Gema Ortega <gemaortegaparra@gmail.com>
2022-09-28 23:39:11 -0300

    agrega lógica

--- contador_app/controllers/controlador.py
+++ contador_app/controllers/controlador.py
@@ -1,13 +1,21 @@
 from contador_app import app
-from flask import render_template
+from flask import render_template, session, redirect
 
 @app.route('/')
 def home():
+    if 'counter' in session:
+        counter=session["counter"]
+        counter = counter +1
+        session["counter"]= counter
+    else:
+        session["counter"]=1
     return render_template('index.html')
 
 @app.route('/destroy_session')
 def destroy_session():
-    return render_template('index.html')
+    session.clear()		
+    #session.pop("counter")		
+    return redirect('/')
 
 
 
